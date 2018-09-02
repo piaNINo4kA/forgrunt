bf78e6bae52fa3b027533cbff57a5ecce2f53d90
Nina Morozova <pianina@MacBook-Pro-Nina.local>
2018-09-02 23:36:46 +0300

    markup without adaptive

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * App entry point.
+ *
+ * @module App
+ */
+
+/** Import initialized-by-default modules/libs */
+import './components/Common';
+import './components/PublicAPI';
+import './components/custom-select'
+
+/** Import page controllers */
+import Home from './pages/Home';
+
+import { currentPage } from './modules/dev/_helpers';
+
+/**
+ * Run appropriate scripts for each page.
+ **/
+switch (currentPage) {
+  /** Home page */
+  case 'home': new Home; break;
+
+  /** No page found */
+  default: console.warn('Undefined page');
+}
